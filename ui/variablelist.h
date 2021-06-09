fd5896d13cfc86648ad6bd966ca9d963534e40d8
Jon Palmisciano <jp@jonpalmisc.com>
2021-06-09 14:01:41 -0400

    VariableListView: Created `VariableListModel` class, moved to model architecture

--- ui/variablelist.h
+++ ui/variablelist.h
@@ -1,19 +1,48 @@
 #pragma once
 
-#include <QtWidgets/QListWidget>
+#include <QtWidgets/QListView>
 #include <QtWidgets/QWidget>
 
 #include "dockhandler.h"
+#include "uitypes.h"
 #include "viewframe.h"
 
-class BINARYNINJAUIAPI VariableListView : public QWidget, public DockContextHandler {
+using VariableList = std::vector<std::pair<BinaryNinja::Variable, BinaryNinja::VariableNameAndType>>;
+
+class BINARYNINJAUIAPI VariableListModel : public QAbstractListModel {
+    Q_OBJECT
+
+    ViewFrame* m_view;
+    BinaryViewRef m_data;
+    FunctionRef m_func;
+    VariableList m_vars;
+
+public:
+    VariableListModel(QWidget* parent, ViewFrame* view, BinaryViewRef data);
+
+    void setFunction(FunctionRef func);
+
+    virtual QVariant data(const QModelIndex& i, int role) const override;
+    virtual QModelIndex index(int row, int col, const QModelIndex& parent = QModelIndex()) const override;
+
+    virtual int columnCount(const QModelIndex& parent = QModelIndex()) const override;
+    virtual int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+
+    Qt::ItemFlags flags(const QModelIndex& index) const override;
+    virtual QVariant headerData(int column, Qt::Orientation orientation, int role) const override;
+};
+
+class BINARYNINJAUIAPI VariableListView
+    : public QWidget,
+      public DockContextHandler {
     Q_OBJECT
     Q_INTERFACES(DockContextHandler)
 
     ViewFrame* m_view;
     BinaryViewRef m_data;
 
-    QListWidget* m_list;
+    VariableListModel* m_listModel;
+    QListView* m_list;
 
 public:
     VariableListView(ViewFrame* view, BinaryViewRef data);
