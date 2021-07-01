6bfd67fb272d38e30810ecfc7c0eceb58c79875e
Jon Palmisciano <jp@jonpalmisc.com>
2021-07-01 16:15:59 -0400

    StackView: Allow variable creation via `CreateStackVariableDialog`

--- ui/stackview.h
+++ ui/stackview.h
@@ -1,8 +1,30 @@
 #pragma once
 
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QLineEdit>
+
 #include "dockhandler.h"
 #include "render.h"
 #include "uitypes.h"
+
+class BINARYNINJAUIAPI CreateStackVariableDialog : public QDialog {
+    Q_OBJECT
+
+    BinaryViewRef m_data;
+    FunctionRef m_func;
+
+    QLineEdit* m_offsetField;
+    QLineEdit* m_nameField;
+    QComboBox* m_typeDropdown;
+
+protected:
+    void accept();
+
+public:
+    CreateStackVariableDialog(QWidget* parent, BinaryViewRef data,
+        FunctionRef func);
+};
 
 class BINARYNINJAUIAPI StackView : public QWidget, public DockContextHandler {
     Q_OBJECT
